1dd65c5262f6335c1525a96a393f4d7b9a54420d
Utkarsh Arora <arorautkarsh2002@gmail.com>
2022-05-09 23:02:30 +0530

    Added a function which shows instructions

--- SourceCode.cpp
+++ SourceCode.cpp
@@ -22,7 +22,7 @@ void showBoard(char board[][SIDE])
 	printf("\t\t\t %c | %c | %c \n\n", board[2][0], board[2][1], board[2][2]);  
 } 
 
-
+//Function to show Instructions
 void showInstructions() 
 { 
 	printf("\nChoose a cell numbered from 1 to 9 as below and play\n\n"); 
